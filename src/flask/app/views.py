08ccc54d1f16094ef54e5fc9d9147f975e4948d6
Jensen, Murray (CMSE, Clayton) <Murray.Jensen@csiro.au>
2018-08-01 02:11:34 +1000

    start defining some modelviews

--- src/flask/app/views.py
+++ src/flask/app/views.py
@@ -18,11 +18,14 @@ from flask import render_template
 from flask_appbuilder import BaseView, SimpleFormView, expose, has_access
 from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
 from flask_appbuilder.forms import DynamicForm
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder.views import ModelView
 from flask_babel import lazy_gettext as _
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 from app import appbuilder, db
+from app.models import Team, Person, TSession, Competition
 
 
 @appbuilder.app.errorhandler(404)
@@ -89,9 +92,69 @@ class MyFormView(SimpleFormView):
         flash(self.message % (form), 'info')
 
 
+class TeamModelView(ModelView):
+    datamodel = SQLAInterface(Team)
+    related_views = [CompetitionModelView, TeamManagerModelView,
+                     CoachModelView, AsstCoachModelView,
+                     CurTSessionModelView, OldTSessionModelView]
+
+
+class CompetitionModelView(ModelView):
+    datamodel = SQLAInterface(Competition)
+
+
+class PersonModelView(ModelView):
+    datamodel = SQLAInterface(Person)
+
+    list_columns = ['name', 'email', 'mobile', 'wwc_number', 'wwc_expiry',
+                    'postal_address', 'photo', 'dob', 'bv_mpd_expiry']
+
+    show_fieldsets = [
+                        (
+                            'Summary',
+                            {'fields': ['name', 'email', 'mobile']}
+                        ),
+                        (
+                            'Personal Info',
+                            {'fields': ['wwc_number', 'wwc_expiry',
+                                        'postal_address', 'dob',
+                                        'bv_mpd_expiry'],
+                             'expanded': False}
+                        ),
+                     ]
+
+
+class TeamManagerModelView(PersonModelView):
+    pass
+
+
+class CoachModelView(PersonModelView):
+    pass
+
+
+class AsstCoachModelView(PersonModelView):
+    pass
+
+
+class TSessionModelView(ModelView):
+    datamodel = SQLAInterface(TSession)
+
+
+class CurTSessionModelView(TSessionModelView):
+    pass
+
+
+class OldTSessionModelView(TSessionModelView):
+    pass
+
+
 appbuilder.add_view(MyView, 'Method1', category='My View')
 appbuilder.add_link('Method2', href='/myview/method2/john', category='My View')
 appbuilder.add_link('Method3', href='/myview/method3/john', category='My View')
 appbuilder.add_view(MyFormView, "My form View", icon="fa-group",
                     label=_('My form View'), category="My Forms",
                     category_icon="fa-cogs")
+
+appbuilder.add_view(TeamModelView, "List Teams",
+                    icon="fa-folder-open-o", category="Teams",
+                    category_icon="fa-envelope")
