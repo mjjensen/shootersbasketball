2bf5106c8eb564c53aee73269578f91ff87855e1
Jensen, Murray (Mineral Resources, Clayton) <Murray.Jensen@csiro.au>
2022-07-29 12:34:58 +1000

    bump clinic term to Term 3, 2022

--- src/sbci/__init__.py
+++ src/sbci/__init__.py
@@ -25,7 +25,7 @@ from urllib3.poolmanager import PoolManager
 provider = os.getenv('PROVIDER', 'PlayHQ')
 association = os.getenv('ASSOCIATION', 'EDJBA')
 season = os.getenv('SEASON', '2022-winter')
-clinicterm = os.getenv('CLINICTERM', '2022-Term2')
+clinicterm = os.getenv('CLINICTERM', '2022-Term3')
 develterm = os.getenv('DEVELTERM', '2022-Term1-Friday')
 
 shootersdir = os.getenv(
