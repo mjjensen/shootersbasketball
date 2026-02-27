bdd0bae06cf5a5c06e3eaa28dc36208c79ec877b
Murray Jensen <mjjensen@users.noreply.github.com>
2026-02-27 23:35:36 +1100

    slight change to dobs in age group calc

--- src/sbci/age_group_calc.py
+++ src/sbci/age_group_calc.py
@@ -170,7 +170,7 @@ def write_age_groups(players, filename, format=OutputFormat.csv,
 
         if incl_dobs:
             # s = date.strftime(dob, '%Y-%m-%d')
-            s = '{} {}'.format(dob.year, 'early' if dob.month <= 6 else 'late')
+            s = '{} {} ½yr'.format(dob.year, '1st' if dob.month <= 6 else '2nd')
             l = len(s)
             row.append(s)
             if l > widths[wi]:
